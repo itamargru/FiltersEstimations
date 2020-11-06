b8ea7c5df6b63990ed57e6079ed913c74e98ac6a
itamargruber <itamargruber@campus.technion.ac.il>
2020-11-06 15:35:48 +0200

    added IMM interaction function

--- Filters/CreateIMMFilter.m
+++ Filters/CreateIMMFilter.m
@@ -1,7 +1,41 @@
-function [outputArg1,outputArg2] = CreateIMMFilter(KalmanFilters)
+function [outputArg1,outputArg2] = CreateIMMFilter(KalmanFilters, TransitionMat, p_init)
 %CREATEIMMFILTER Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+IMM.size = size(KalmanFilters);
+IMM.TransitionMat = TransitionMat;
+IMM.p_prior = p_init;
+IMM.p_posterior = p_init;
+
 end
 
+function [IMM, x_interaction, P_interaction] = interaction(IMM)
+    % number of Kalman filters
+    n = IMM.size;
+    % number of x state dimention
+    d = size(IMM.KalmanFilters{0}.x_posterior,1);
+    H = IMM.TransitionMat
+    IMM.p_prior = H * IMM.p_posterior;
+    
+    x_interaction = zeros(d, n);
+    parfor ii = 1:n
+        for jj = 1:n
+            x_posterior = IMM.KalmanFilters{jj}.x_posterior;
+            x_toSum = (H(ii, jj) * IMM.p_posterior(jj) / IMM.p_prior(ii)) * x_posterior;
+            x_interaction(:, ii) = x_interaction(:, ii) + x_toSum;
+        end
+    end
+    
+    P_interaction = zeros(d, d, n);
+    parfor ii = 1:n
+        for jj = 1:n
+            x_posterior = IMM.KalmanFilters{jj}.x_posterior;
+            x_diff = x_posterior - x_interaction(:, ii);
+            x_diff_cov = x_diff * x_diff';
+            
+            P_posterior = IMM.KalmanFilters{jj}.P_posterior;
+            factor = H(ii, jj) * IMM.p_posterior(jj) / IMM.p_prior(ii);
+            P_toSum = factor * (P_posterior + x_diff_cov);
+            P_interaction(:, :, ii) = P_interaction(:, :, ii) + P_toSum;
+        end
+    end
+end
--- Filters/CreateKalmanFilter.m
+++ Filters/CreateKalmanFilter.m
@@ -7,23 +7,23 @@ function [KM] = CreateKalmanFilter(A, H, G, Q, R, x_prior, P_prior)
     
     KM.x_prior = x_prior;
     KM.P_prior = P_prior;
-    KM.x_postirior = zeros( size(x_prior) );
-    KM.P_postirior = zeros( size(P_prior) );
+    KM.x_posterior = zeros( size(x_prior) );
+    KM.P_posterior = zeros( size(P_prior) );
     
     KM.update = @(KM, z) update(KM, z, KM.x_prior, KM.P_prior);
-    KM.predict = @(KM) predict(KM, KM.x_postirior, KM.P_postirior);
+    KM.predict = @(KM) predict(KM, KM.x_posterior, KM.P_posterior);
 
 end
 
 %update
-function [x_postirior,P_postirior] = update(KM, z, x_prior, P_prior)
+function [x_posterior,P_posterior] = update(KM, z, x_prior, P_prior)
     K = P_prior * KM.H.' * (KM.H * P_prior * KM.H.' + KM.R)^-1;
-    x_postirior = x_prior + K * (z - KM.H * x_prior);
-    P_postirior = (eye(2) - K * KM.H) * P_prior;
+    x_posterior = x_prior + K * (z - KM.H * x_prior);
+    P_posterior = (eye(2) - K * KM.H) * P_prior;
 end
 
 %predict
-function [x_prior, P_prior] = predict(KM, x_postirior, P_postirior)
-    x_prior = KM.A * x_postirior;
-    P_prior = KM.A * P_postirior * KM.A.' + KM.Q;
+function [x_prior, P_prior] = predict(KM, x_posterior, P_posterior)
+    x_prior = KM.A * x_posterior;
+    P_prior = KM.A * P_posterior * KM.A.' + KM.Q;
 end
