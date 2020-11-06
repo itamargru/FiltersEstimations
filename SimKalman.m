044c2ad1f24446742ff707c810c7eee5ca11cbef
Abed Kadry <kadrya@campus.technion.ac.il>
2020-11-06 14:02:22 +0200

    removed unnecessary lines

--- SimKalman.m
+++ SimKalman.m
@@ -3,9 +3,6 @@ close all;
 clc;
 
 %% Trajectories
-%trajectory_part1 = linspace(0,20000,201);
-%trajectory_part2 = linspace(19900,-10000,300);
-%trajectory = cat(2, trajectory_part1, trajectory_part2);
 
 velocity_1 = 100 * ones(1,200);
 velocity_2 = -100 * ones(1,300);
@@ -27,11 +24,11 @@ T = 1; %second
 
 H = [1, 0]; 
 G = [0.5*T^2; T]; %m/s^2
-Q = 1 * G * G.';
+Q = 1 * (G * G.');
 R = 1e3^2; %measurement variance
 A = [1, T; 0, 1];
 
-KM = CreateKalmanFilter(A,H,G,Q,R,x_prior_0, P_prior_0)
+KM = CreateKalmanFilter(A,H,G,Q,R,x_prior_0, P_prior_0);
 
 
 %% Simulation
@@ -61,7 +58,7 @@ ylabel("Error[m]");
 xlabel("Iteration");
 xlim([1, 501]);
 title("Error Estimated Position");
-saveas(fig1, fullfile(root, "results", "Position.png"));
+saveas(fig1, fullfile(root, "Results", "Position.png"));
 
 fig2 = figure(2);
 subplot(2, 1, 1); hold on;
@@ -79,7 +76,7 @@ ylabel("Error[m/sec]");
 xlabel("Iteration");
 xlim([1, 501]);
 title("Error Estimated Velocity");
-saveas(fig2, fullfile(root,"results", "Velocity.png"));
+saveas(fig2, fullfile(root,"Results", "Velocity.png"));
 
 
 fig3 = figure(3);
@@ -87,35 +84,13 @@ hold on;
 title("innovation process");
 plot(inn);
 xlim([1, 501]);
-saveas(fig3, fullfile(root, "results", "Innovation.png"));
+saveas(fig3, fullfile(root, "Results", "Innovation.png"));
 
 
-% 
-% figure(4)
-% hold on;
-% plot(estimation_err, 'r');
-% plot(measurement_err, 'g');
+%% Kalman Process
 
-%% Kalman Functions
-
-%update
-function [x_postirior,P_postirior] = update(KM, x_prior, P_prior, z)
-    K = P_prior * KM.H.' * (KM.H * P_prior * KM.H.' + KM.R)^-1;
-    x_postirior = x_prior + K * (z - KM.H * x_prior);
-    P_postirior = (eye(2) - K * KM.H) * P_prior;
-end
-
-%predict
-function [x_prior, P_prior] = predict(KM, x_postirior, P_postirior)
-    x_prior = KM.A * x_postirior;
-    P_prior = KM.A * P_postirior * KM.A.' + KM.Q;
-end
-
-%process
 function [xs_prior, xs_postirior, Ps_prior, Ps_postirior] = KalmanProcess(KM, measurements)
     num_measurements = size(measurements, 2);
-    x_prior = KM.x_prior;
-    P_prior = KM.P_prior;
     xs_prior = zeros(num_measurements, 2);
     xs_postirior = zeros(num_measurements, 2);
     Ps_prior = zeros(num_measurements, 2, 2);
