4e9ab4716d4044e546cadf3af7e63fd81186e9f7
Abed Kadry <kadrya@campus.technion.ac.il>
2021-03-10 14:38:34 +0200

    made trajectory into a function

---
+++ trajectory.m
@@ -0,0 +1,22 @@
+%% Trajectory
+% creates a trajectory corresponding to a certain desirable
+% manouvering index (from 0.1 to 2.5) by choosing the relevant 
+% acceleration given rms error of sigma = 100 and T = 5.
+function [positions, velocities] = trajectory(MI)
+    x0 = -2.5e4;
+    v0 = 120;
+    dt = 60;
+    a = 4*MI;
+    acc = [0 -a 0 a 0 a 0];
+    dt_vel = ones(1,dt).'*acc; 
+    dt_vel = dt_vel(:).';
+    dt_vel(1) = [];
+    vel = cumsum([v0, dt_vel]);
+    velocities = vel;
+    vel(1) = [];
+    X = cumsum([x0, vel]);
+    positions = X;
+end
+
+
+
