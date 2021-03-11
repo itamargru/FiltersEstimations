dad140c8bae7225704726dc7e957926ff93a13ae
Abed Kadry <kadrya@campus.technion.ac.il>
2021-03-11 10:39:37 +0200

    expanded trajectory function to use measurement var and sampling period

--- trajectory.m
+++ trajectory.m
@@ -1,12 +1,12 @@
 %% Trajectory
 % creates a trajectory corresponding to a certain desirable
 % manouvering index (from 0.1 to 2.5) by choosing the relevant 
-% acceleration given rms error of sigma = 100 and T = 5.
-function [positions, velocities] = trajectory(MI)
+% acceleration given rms error of sigma  and T .
+function [positions, velocities] = trajectory(MI, meas_var, T)
     x0 = -2.5e4;
     v0 = 120;
     dt = 60;
-    a = 4*MI;
+    a = ((meas_var^0.5)/(T^2))*MI;
     acc = [0 -a 0 a 0 a 0];
     dt_vel = ones(1,dt).'*acc; 
     dt_vel = dt_vel(:).';
@@ -17,6 +17,8 @@ function [positions, velocities] = trajectory(MI)
     X = cumsum([x0, vel]);
     positions = X;
 end
+
+
 
 
 
